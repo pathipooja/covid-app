f1e7b8cdb98e6a5cd67063a7d871a6abac28d4af
pathipooja <pathipooja@gmail.com>
2020-05-12 15:03:45 +0530

    database added

--- covid_status/views.py
+++ covid_status/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from requests import get
+import requests
 from .models import Country
 from .forms import CountryForm
 
@@ -7,32 +7,41 @@ from .forms import CountryForm
 def index(request):
     # url='https://api.covid19api.com/live/country/{}'
     url = 'http://covid19-india-adhikansh.herokuapp.com/state/{}'
-
     if request.method == 'POST':
         form = CountryForm(request.POST)
-        if form.is_valid():
-            a = form.cleaned_data['name']
-        print(a)
-        r = get(url.format(a))
-        if r.status_code == 200:
-            r = r.json()
-            country_status = {
-                'country': a,
-                'confirmed_cases': r['data'][0]['total'],
-                'death_cases': r['data'][0]['death'],
-                'recovered_cases': r['data'][0]['cured'],
-                'active_cases': r['data'][0]['confirmed'],
-            }
-            print(country_status)
-            context = {'country_status': country_status, 'form': form}
-            return render(request, 'covid_status/covid_status.html', context)
-    country_status = {
-        'country': " ",
-        'confirmed_cases': 0,
-        'death_cases': 0,
-        'recovered_cases': 0,
-        'active_cases': 0,
-    }
+        res = form.data['name']
+        form.save()
+
     form = CountryForm()
-    context = {'country_status': country_status, 'form': form}
+    countries = Country.objects.all()
+    country_data = []
+    i=0
+    for country in countries:
+        i+=1
+        r = requests.get(url.format(country)).json()
+        # if r.status_code == 200:
+        country_status = {
+            'country': country.name,
+            'confirmed_cases': r['data'][0]['total'],
+            'death_cases': r['data'][0]['death'],
+            'recovered_cases': r['data'][0]['cured'],
+            'active_cases': r['data'][0]['confirmed'],
+        }
+    if i>0:
+        country_data.append(country_status)
+    '''else:
+            country_status = {
+                'country': " ",
+                'confirmed_cases': 0,
+                'death_cases': 0,
+                'recovered_cases': 0,
+                'active_cases': 0,
+            }
+'''
+    if i==0:
+        context = {'form': form}
+    elif request.method == 'POST':
+        context = {'country_data': country_data, 'res': res, 'form': form}
+    else:
+        context = {'country_data': country_data, 'res': 'Last Search', 'form': form}
     return render(request, 'covid_status/covid_status.html', context)
